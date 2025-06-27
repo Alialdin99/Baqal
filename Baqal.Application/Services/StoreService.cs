57207f4c016e56caa1065c6dda8b45ce9660ec3e
Alialdeen <alialdin.mohamad@gmail.com>
2025-06-27 22:43:09 +0300

    added a store service, a store addition DTO and a job addition methon inside the store service

---
+++ Baqal.Application/Services/StoreService.cs
@@ -0,0 +1,34 @@
+﻿using Baqal.Application.DTOs;
+using Baqal.DataAccess.Interfaces;
+using Baqal.Entities.Models;
+using Microsoft.VisualBasic;
+
+namespace Baqal.Application.Services
+{
+    public class StoreService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public StoreService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<bool> AddStore(AddStoreDTO addStoreDTO)
+        {
+            var store = new Store()
+            {
+                Name = addStoreDTO.Name,
+                Address = addStoreDTO.Address,
+                City = addStoreDTO.City,
+                Governorate = addStoreDTO.Governorate,
+            };
+            
+            await _unitOfWork.Stores.AddAsync(store);
+            await _unitOfWork.Save();
+            return true;
+        }   
+
+    }
+}
+
